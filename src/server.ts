e1bf184b03c61d85e850c19b36716eed67fd2443
Aura <111578898+analaurabrito@users.noreply.github.com>
2024-09-10 12:14:16 -0300

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import UserRouter from "./routes/UserRoutes";
+import CommentRouter from "./routes/CommentRoutes";
+import PostRouter from "./routes/PostRoutes";
+
+const app = express();
+app.use(express.json());
+
+app.use(UserRouter);
+app.use(PostRouter);
+app.use(CommentRouter);
+
+app.listen(3000, function () {
+  console.log("Servidor rodando na porta 3000");
+});
